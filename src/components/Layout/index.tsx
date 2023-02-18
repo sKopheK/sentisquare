a4e2cd419c97aca5430ca2dcf682b0c215b591cf
skophek <libor@lipe.cz>
2023-02-19 00:25:32 +0100

    app title w/ version

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import { FC, Suspense } from 'react';
 
 import cx from 'classnames';
-import { Card, Spinner } from 'react-bootstrap';
+import { Badge, Card, Spinner } from 'react-bootstrap';
 
 import ResultList from 'components/ResultList';
 
@@ -10,7 +10,12 @@ import styles from './styles.module.scss';
 const Layout: FC = () => {
   return (
     <main className={cx(styles.main, 'p-3')}>
-      <h1>Sentisquare Code Challenge</h1>
+      <h1 className="mb-3 d-flex justify-content-between">
+        Sentisquare Code Challenge
+        <Badge bg="info" className="ms-3">
+          v{process.env.REACT_APP_VERSION}
+        </Badge>
+      </h1>
       <Card>
         <Card.Header className="h2">Results</Card.Header>
         <Card.Body>
