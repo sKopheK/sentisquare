1e9c221cbaf9c7aa48ce4fb80685a036a2fcfa69
skophek <libor@lipe.cz>
2023-02-21 21:00:29 +0100

    Graph list layout update

--- src/components/Graphs/List/index.tsx
+++ src/components/Graphs/List/index.tsx
@@ -1,20 +1,26 @@
 import { FC } from 'react';
 
-import { CardGroup } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 import { GraphListProps } from './types';
 
-import { getAllEntities } from '../helpers';
+import { getAllEntities } from 'components/Graphs/helpers';
 
-import FrequencyGraph from '../Frequency';
+import ConfidenceGraph from 'components/Graphs/Confidence';
+import FrequencyGraph from 'components/Graphs/Frequency';
 
 const GraphList: FC<GraphListProps> = ({ results }) => {
   const entities = getAllEntities(results);
 
   return (
-    <CardGroup>
-      <FrequencyGraph entities={entities} />
-    </CardGroup>
+    <Row xs={1} lg={2} className="g-4">
+      <Col>
+        <FrequencyGraph entities={entities} />
+      </Col>
+      <Col>
+        <ConfidenceGraph entities={entities} />
+      </Col>
+    </Row>
   );
 };
 
