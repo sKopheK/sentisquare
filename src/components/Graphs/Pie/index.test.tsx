a5344988c36720762eb138c490261444b8554f44
skophek <libor@lipe.cz>
2023-03-05 22:10:34 +0100

    PieGraph - no data to display disclaimer

--- src/components/Graphs/Pie/index.test.tsx
+++ src/components/Graphs/Pie/index.test.tsx
@@ -5,4 +5,9 @@ describe('PieGraph component', () => {
   it('should render component', () => {
     expect(() => render(<PieGraph data={[]} />)).not.toThrow();
   });
+
+  it('should render empty data disclaimer', () => {
+    const { getByText } = render(<PieGraph data={[]} />);
+    expect(getByText('No data to display.')).toBeInTheDocument();
+  });
 });
--- src/components/Graphs/Pie/index.tsx
+++ src/components/Graphs/Pie/index.tsx
@@ -17,31 +17,39 @@ import { DEFAULT_GRAPH_HEIGHT } from '../constants';
 const PieGraph: FC<PieGraphProps> = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={DEFAULT_GRAPH_HEIGHT}>
-      <PieChart>
-        <Pie
-          data={data}
-          dataKey="value"
-          cx="50%"
-          cy="50%"
-          outerRadius={70}
-          label
-          labelLine
-        >
-          {data.map((_, index) => (
-            <Cell
-              key={`cell-${index}`}
-              fill={GRAPH_COLORS[index % GRAPH_COLORS.length]}
-            />
-          ))}
-        </Pie>
-        <Legend
-          align="left"
-          layout="vertical"
-          verticalAlign="middle"
-          iconType="wye"
-        />
-        <Tooltip />
-      </PieChart>
+      {data.length === 0 ? (
+        <div style={{ height: DEFAULT_GRAPH_HEIGHT + 'px' }}>
+          <div className="position-absolute top-50 start-50 translate-middle">
+            No data to display.
+          </div>
+        </div>
+      ) : (
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="value"
+            cx="50%"
+            cy="50%"
+            outerRadius={70}
+            label
+            labelLine
+          >
+            {data.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={GRAPH_COLORS[index % GRAPH_COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Legend
+            align="left"
+            layout="vertical"
+            verticalAlign="middle"
+            iconType="wye"
+          />
+          <Tooltip />
+        </PieChart>
+      )}
     </ResponsiveContainer>
   );
 };
